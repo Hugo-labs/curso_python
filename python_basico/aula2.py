24e36cee6c7726e1326c279120d66ff55ff1c6d1
Victor Hugo <victorhugo.prog@gmail.com>
2025-07-20 12:41:05 -0300

    introducao a python

---
+++ python_basico/aula2.py
@@ -0,0 +1,8 @@
+# sep='' para definir o sepador dos argumentos
+#  end='' para definir o que será feito após o final do argumento
+# \n para quebrar uma linha
+
+
+print(12, 34, sep='**', end='\n**')
+print(56, 78, sep='--')
+print(f'Olá Eu me chamo \nVictor')

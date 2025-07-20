d3273b8d1c08995d0c63e5745d11629675e227c2
Victor Hugo <victorhugo.prog@gmail.com>
2025-07-20 19:13:39 -0300

    introducao a strings

---
+++ python_basico/aula3.py
@@ -0,0 +1,19 @@
+"""
+Python linguagem de programação
+Tipagem = dinâmica / forte
+Str -> string -> texto
+Strings são textos que estão entre áspas
+"""
+
+# Aspas simples
+print('Olá galera')
+
+# Aspas duplas
+print("Podemos usar áspas duplas também")
+
+# Escape
+print('Caractere \'de escape')
+
+# r
+# serve para mostrar o caractere de escape ;-;
+print(r'Caractere \'de escape')

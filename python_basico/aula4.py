fc7ff2fd58aeb1355637f7882c55c6422591e95c
Victor Hugo <victorhugo.prog@gmail.com>
2025-07-21 13:47:41 -0300

    introducao a int e float

---
+++ python_basico/aula4.py
@@ -0,0 +1,23 @@
+# Tipos int e float
+# int -> numero inteiro 
+# int representa numeros positivos e negativos
+print(22)
+print(-17)
+print(0)
+print('-' * 18)
+
+
+# float -> numero com ponto flutuante
+# float -> representa numeros positivos e negativos com ponto flutuante
+print(22.1)
+print(-17.4)
+print(0.0)
+print('-' * 18)
+
+
+# type mostra o tipo que o python inferiu ao valor
+print(type(12))
+print(type(-7))
+print(type(0.0))
+print(type(-2.52))
+print(type('17'))

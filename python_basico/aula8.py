cafe06b9080bc83bbedaabb36e2193b10405afd5
Victor Hugo <victorhugo.prog@gmail.com>
2025-07-22 19:35:24 -0300

    operadores aritmeticos

---
+++ python_basico/aula8.py
@@ -0,0 +1,24 @@
+# Operadores aritméticos
+adicao = 2 + 2
+print(f'Soma: {adicao}')
+
+subtracao = 10 - 4
+print(f'Subtração: {subtracao}')
+
+multiplicacao = 5 * 5
+print(f'Multiplicação: {multiplicacao}')
+
+divisao = 10 / 2.2
+print(f'Divisão: {divisao}')
+
+divisao_inteira = 10 // 2.2
+print(f'Divisão inteira: {divisao_inteira}')
+
+resto_divisao = 10 % 3
+print(f'Resto da divisão: {resto_divisao}')
+
+exponenciacao = 2 ** 3
+print(f'Exponenciação: {exponenciacao}')
+
+
+

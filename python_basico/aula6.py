744fba11de0e4a8fcad993abf97a4ec133078f3a
Victor Hugo <victorhugo.prog@gmail.com>
2025-07-22 17:27:54 -0300

    introducao a variaveis

---
+++ python_basico/aula6.py
@@ -0,0 +1,12 @@
+# Variáveis são usadas para salvar dados na memória do computador
+# Iniciar variáveis com letras minúsculas, podendo usar também números e underline _
+# Sinal de = é para atribuir um valor a variável
+
+# nome_completo = 'Victor Hugo Barcellos'
+# idade = 20 + 2
+# print(nome_completo, idade)
+
+nome = 'Victor'
+idade = 17
+maior_idade = idade >=18
+print(f'{nome} é maior de idade? {maior_idade}')

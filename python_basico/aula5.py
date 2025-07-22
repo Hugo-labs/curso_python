881ffae032983f02830587c8c5c649eddcf754f4
Victor Hugo <victorhugo.prog@gmail.com>
2025-07-21 23:32:09 -0300

    introducao a bool

---
+++ python_basico/aula5.py
@@ -0,0 +1,7 @@
+# Tipo bool (bolean)
+# Ao questionar o programa existe apenas duas resposta
+# True ou false
+print(10 == 10)
+print(17 == 22)
+print('Victor' == 'Victor')
+print('Victor' == 'Vitor')

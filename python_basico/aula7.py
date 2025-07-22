ebfff91f830bdab7dfcd86136f19dc77639ca3d9
Victor Hugo <victorhugo.prog@gmail.com>
2025-07-22 17:46:28 -0300

    exercicio basico

---
+++ python_basico/aula7.py
@@ -0,0 +1,14 @@
+nome = 'Victor'
+sobre_nome = 'Hugo'
+idade = 22
+ano_nascimento = 2025 - idade
+maior_idade = idade >= 18
+altura = 1.68
+
+
+print(f'Nome: {nome}')
+print(f'Sobre nome: {sobre_nome}')
+print(f'Idade: {idade}')
+print(f'Ano de nascimento: {ano_nascimento}')
+print(f'É maior de idade: {maior_idade}')
+print(f'Altura em metros: {altura}m')

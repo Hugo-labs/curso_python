a1575cf2406fc58c8a9b432d928d4471169e9baf
Victor Hugo <victorhugo.prog@gmail.com>
2025-07-24 19:27:02 -0300

    introducao ao input

---
+++ python_basico/aula12.py
@@ -0,0 +1,10 @@
+nome = input('Digite o seu nome: ')
+print(f'Olá {nome}!')
+
+numero_1 = (input('Número 1: '))
+numero_2 = (input('Número 2: '))
+
+int_numero_1 = int(numero_1)
+int_numero_2 = int(numero_2)
+
+print(f'A soma entre {numero_1} + {numero_2} = {numero_1 + numero_2}')

a838b4ac50efc558659260b8614de8630938eed9
Victor Hugo <victorhugo.prog@gmail.com>
2025-07-30 22:06:03 -0300

    operadores de comparacao

---
+++ python_basico/aula14.py
@@ -0,0 +1,15 @@
+# Operadores de comparação
+
+maior = 2 > 1
+maior_ou_igual = 2 >= 2
+menor = 1 < 2
+menor_ou_igual = 1 <= 1
+igual = 'a' == 'a'
+diferente = 'a' != 'b'
+
+print(f'2 > 1 = {maior}')
+print(f'2 >= 2 = {maior_ou_igual}')
+print(f'1 < 2 = {menor}')
+print(f'1 <= 1 = {menor_ou_igual}')
+print(f'"a" = "a" = {igual}')
+print(f'"a" != "b" = {diferente}')

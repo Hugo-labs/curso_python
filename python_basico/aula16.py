6fa3c544f8eb2d7cc98f0947e6aef83a1ecfa504
Victor Hugo <victorhugo.prog@gmail.com>
2025-08-24 06:52:35 -0300

    operador logico and

---
+++ python_basico/aula16.py
@@ -0,0 +1,16 @@
+"""
+Operadores lógicos
+and - Todas as condições precisam ser verdadeiras
+Se qualquer valor for dado como falso a expressão inteira receberá este valor
+São considerados falsy
+0 0.0 '' False
+e também o tipo None que é para representar um "não valor"
+"""
+senha = '123456'
+entrada = input('[E]ntrar [S]air: ').upper()
+senha_digitada = input('Senha: ')
+
+if entrada == 'E' and senha_digitada == senha:
+    print('Você entrou')
+else:
+    print('Você saiu')

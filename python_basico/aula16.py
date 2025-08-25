95026d82da8a104efb604d4a08dee4afa08581e7
Victor Hugo <victorhugo.prog@gmail.com>
2025-08-25 05:51:53 -0300

    operador logico and

--- python_basico/aula16.py
+++ python_basico/aula16.py
@@ -6,11 +6,16 @@ São considerados falsy
 0 0.0 '' False
 e também o tipo None que é para representar um "não valor"
 """
-senha = '123456'
-entrada = input('[E]ntrar [S]air: ').upper()
-senha_digitada = input('Senha: ')
+# senha = '123456'
+# entrada = input('[E]ntrar [S]air: ').upper()
+# senha_digitada = input('Senha: ')
 
-if entrada == 'E' and senha_digitada == senha:
-    print('Você entrou')
-else:
-    print('Você saiu')
+# if entrada == 'E' and senha_digitada == senha:
+#     print('Você entrou')
+# else:
+#     print('Você saiu')
+
+print(True and True and False and True )
+print(True and True and True)
+print(bool(' '))
+print(bool(' '))

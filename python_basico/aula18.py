0a951ff86dde98c622d66bbce13377db70d95a90
Victor Hugo <victorhugo.prog@gmail.com>
2025-08-25 15:37:19 -0300

    operador logico not

---
+++ python_basico/aula18.py
@@ -0,0 +1,17 @@
+"""
+Operador lógico not
+Usado para inverter expressões
+not True = False
+not False = True
+"""
+
+# senha = '123'
+# senha_digitada = input('Senha: ')
+
+# if senha_digitada != senha:
+#     print('Senha incorreta')
+# else:
+#     print('Senha correta')
+
+print(not True)
+print(not '')

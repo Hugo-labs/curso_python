565585970cec2b8fe7f946107036d496173c4c7d
Victor Hugo <victorhugo.prog@gmail.com>
2025-08-26 05:13:25 -0300

    flags

---
+++ python_basico/aula26.py
@@ -0,0 +1,14 @@
+"""
+Flags - Usado para marcar um local
+"""
+
+condicao = False
+passou_no_if = None  # Flag
+
+if condicao:
+    passou_no_if = True
+    print('Faça algo')
+else:
+    print('Não faça algo')
+print(passou_no_if)
+ 

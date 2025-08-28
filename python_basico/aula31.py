8f68d93e1f02e54fb52ac86a099fc7363ab67f53
Victor Hugo <victorhugo.prog@gmail.com>
2025-08-28 05:46:49 -0300

    introducao ao while

---
+++ python_basico/aula31.py
@@ -0,0 +1,13 @@
+"""
+Repetições
+while
+Executa uma repetição enquanto uma condição for verdadeira
+"""
+condicao = True
+
+while condicao:
+    nome = input('Qual é o seu nome? ')
+    print(f'Bom dia {nome}')
+    break
+
+print('Fim')

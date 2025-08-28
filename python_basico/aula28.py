aa5466d3b8d45e4f91c92d8d9cb07a59a147e2a2
Victor Hugo <victorhugo.prog@gmail.com>
2025-08-28 00:57:01 -0300

    exercicio saudacao com base no horario inteiro

--- python_basico/aula28.py
+++ python_basico/aula28.py
@@ -1,15 +1,16 @@
 horario = input('Que horas são: ')
 horario_int = None
 
-if horario.isdigit():
-    horario_int = int(horario)
 
 try:
+    horario_int = int(horario)
     if 0 <= horario_int <= 11:
         print('Bom dia')
     elif 12 <= horario_int <= 17:
         print('Boa tarde')
     elif 18 <= horario_int <= 23:
         print('Boa noite')
+    else:
+        print('Informe um horário válido')
 except:
-    print('Informe o horário corretamente')
+    print('Informe o horário corretamente') 

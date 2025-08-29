48dc6604f550adcfc41152c8887d420808f29926
Victor Hugo <victorhugo.prog@gmail.com>
2025-08-29 16:25:24 -0300

    operadores de atribuicao

---
+++ python_basico/aula33.py
@@ -0,0 +1,23 @@
+"""
+Operadores de atribuição
+= += -= *= **= /= //= %=
+"""
+
+contador = 2
+
+# contador = contador + 1  # contador == 3
+# contador += 1
+
+# contador -= 1  # contador == 1
+
+# contador *= 2  # contador == 4
+
+# contador **= 3  # contador == 8
+
+# contador /= 2  # contador == 1.0
+
+# contador //= 2  # contador == 1
+
+# contador %= 2  # contador == 0
+
+print(contador)
